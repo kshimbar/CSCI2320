5f5e5676458fd56b602b23328b9adc7265304a21
kshimbar <kshimbar@trinity.edu>
2021-08-27 10:33:07 -0500

    Aug 27th

--- InClass.cpp
+++ InClass.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 //g++ -std=c++11 -Wall InClass.cpp -o inclass
+void swapval(int* x, int* y);
+void swapref(int &x, int &y);
 
 int main(){
     //Aug 25th
     std::cout << "Hello world!\n";
+}
+
+//Aug 27th
+void swapval(int*x, int*y){
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+void swapref(int &x, int &y){
+    int a = x;
+    x = y;
+    y = a;
+}
+
+struct Point{
+    double x;
+    double y;
 }
