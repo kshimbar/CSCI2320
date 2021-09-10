1668314f759770a8de919448bad565d31a804256
2020-08-18 <kshimbar@cs-xena09.trinity.local>
2021-09-09 20:30:38 -0500

    final

--- ArrayQueue.h
+++ ArrayQueue.h
@@ -1,82 +1,81 @@
 #include "Queue.h"
 
 template<typename T>
-class ArrayQueue : public Queue<T> {
-public:
-    explicit ArrayQueue(int s);
-    ArrayQueue();
-    ArrayQueue(const ArrayQueue &a) : elem{new T[sz]}, sz{a.sz},front{a.front},back{a.back}{
-        for(int i = 0; i != sz; ++i){
-            elem[i] = a.elem[i];
-        }
-    }
-    ArrayQueue &operator=(const ArrayQueue<T> &a){
-        T* p = new T[a.sz];
-        for(int i = 0; i != a.sz; i++){
-            p[i] = a.elem[i];
-        }
-        delete[] elem;
-        elem = p;
-        sz = a.sz;
-        front = a.front;
-        back = a.back;
-    }
-    ~ArrayQueue(){delete[] elem;}
-    void enqueue(const T &t);
-    T dequeue();
-    T peek() const;
-    bool isEmpty() const;
-
+class ArrayQueue : public Queue<T>{
 private:
-    T *elem;
-    int sz;
-    int front;
-    int back;
+	T* elem;
+	int sz;
+	int front;
+	int back;
+public:
+	explicit ArrayQueue(int s);
+	ArrayQueue();
+	ArrayQueue(const ArrayQueue &a);
+	ArrayQueue &operator=(const ArrayQueue<T> &a);
+	~ArrayQueue();
+	void enqueue(const T &t);
+	T dequeue();
+	T peek() const;
+	bool isEmpty() const;
 };
 
 template<typename T>
-ArrayQueue<T>::ArrayQueue(int s):elem{new T[s]},sz{s},front{0},back{0}{}
-
-template<typename T>
-T ArrayQueue<T>::peek() const {
-    return elem[front];
-}
-
-template<typename T>
-bool ArrayQueue<T>::isEmpty() const {
-    return (front == back);
-}
+ArrayQueue<T>::ArrayQueue():elem{new T[10]},sz{10}, front{0}, back{0} {}
 
 template<typename T>
 void ArrayQueue<T>::enqueue(const T &t) {
-    if ((back + 1) % sz == front) {
-        T *tmp = new T[sz * 2];
-        for (int ind = 0; ind <= sz; ++ind) {
-            tmp[ind] = elem[ind];
-        }
-        delete[] elem;
-        elem = tmp;
-        sz *= 2;
-        back = (back + 1) % sz;
-    } else if (isEmpty()) {
-        front = 0;
-        back = 0;
-    } else {
-        back = (back + 1) % sz;
-    }
-    elem[back] = t;
+	if (back + 1 % sz == front) {
+		T *tmp = new T[sz * 2];
+		for (int i = 0; i <= sz; ++i)
+			tmp[i] = elem[i];
+		delete[] elem;
+		elem = tmp;
+		sz *= 2;
+		back = (back + 1) % sz;
+	} else if(!isEmpty()){
+		back = (back + 1) % sz;
+	}
+	elem[back] = t;
 }
 
 template<typename T>
 T ArrayQueue<T>::dequeue() {
-    T ret;
-    if (isEmpty()) {
-        return 0;
-    } else if (front == back) {
-        ret = elem[front];
-    } else {
-        ret = elem[front];
-        front = (front + 1) % sz;
-    }
-    return ret;
+	T ret;
+	if (isEmpty()) {
+		return 0;
+	} else {
+		ret = elem[front - 1];
+		front -= 1;
+	}
+	return ret;
+}
+
+template<typename T>
+T ArrayQueue<T>::peek() const {
+	return elem[front];
+}
+
+template<typename T>
+bool ArrayQueue<T>::isEmpty() const {
+	return (front == back);
+}
+template<typename T>
+ArrayQueue<T>::~ArrayQueue(){
+	delete[] elem;
+}
+template <typename T>
+ArrayQueue<T> &ArrayQueue<T>::operator=(const ArrayQueue<T> &a){
+	T*p = new T[a.sz];
+	for(int i = 0; i != a.sz; ++i)
+		p[i] = elem[i];
+	delete[] elem;
+	elem = p;
+	sz = a.sz;
+	front = a.front;
+	back= a.back;
+}
+template <typename T>
+ArrayQueue<T>::ArrayQueue(const ArrayQueue<T> &a):elem{a.elem},sz{a.sz},front{a.front},back{a.back}{
+for(int i = 0; i != a.sz; ++i)
+elem[i] = a.elem[i];
 }
--- ArrayStack.h
+++ ArrayStack.h
@@ -1,53 +1,64 @@
 #include "Stack.h"
+
 template<typename T>
-class ArrayStack : public Stack<T>{
-    private:
-        T* elem;
-        int sz;
-        int top;
-        void print() const;
-    public:
-    	ArrayStack() : elem{new T[10]},sz{10},top{0} {};;
-	ArrayStack(int s);
+class ArrayStack : public Stack<T> {
+public:
+	ArrayStack(): elem{new T[10]}, sz{10}, top{0} {};
 	ArrayStack(const ArrayStack &a);
-    	ArrayStack& operator=(const ArrayStack& a){
-        	T* p = new T[a.sz];
-        	for(int i = 0; i != a.sz; ++i)
-            		p[i] = a.elem[i];
-        	delete[] elem;
-        	elem = p;
-        	sz = a.sz;
-        	top = a.top;
-    	}
+	ArrayStack &operator=(const ArrayStack &a);
 	~ArrayStack() {delete[] elem;}
 	void push(const T &t);
 	T pop();
-	T peek() const { return elem[top]; }
-	bool isEmpty() const { return top == 0; }
+	T peek() const;
+	bool isEmpty() const;
+private:
+	T *elem;
+	int sz;
+	int top;
 };
 
 template<typename T>
-ArrayStack<T>::ArrayStack(const ArrayStack &a):elem{new T[sz]},sz{a.sz},top{a.top} {
-    	for(int i = 0; i != sz; i++)
-        	elem[i] = a.elem[i];
+ArrayStack<T>::ArrayStack(const ArrayStack &a):elem{new T[sz]},sz{a.sz}, top{a.top} {
+	for (int i = 0; i != sz; i++)
+		elem[i] = a.elem[i];
 }
 
 template<typename T>
-void ArrayStack<T>::push(const T &t){
-    if(top == sz){
-       T* tmp = new T[sz*2];
-       for(int i = 0; i != sz; ++i){
-            tmp[i] = elem[i];
-        }
-        delete[] elem;
-        elem = tmp;
-        sz *= 2;
-    }
-    elem[top] = t;
-    top += 1;
+ArrayStack<T> &ArrayStack<T>::operator=(const ArrayStack &a) {
+	T *p = new T[a.sz];
+	for (int i = 0; i != a.sz; ++i)
+		p[i] = a.elem[i];
+	delete[] elem;
+	elem = p;
+	sz = a.sz;
+	top = a.top;
 }
 
-template <typename T>
+template<typename T>
+void ArrayStack<T>::push(const T &i) {
+	if (top == sz) {
+		T *tmp = new T[sz * 2];
+		for (int ind = 0; ind != sz; ind++)
+			tmp[ind] = elem[ind];
+		delete[] elem;
+		elem = tmp;
+		sz *= 2;
+	}
+	elem[top] = i;
+	++top;
+}
+
+template<typename T>
 T ArrayStack<T>::pop(){
-    return elem[--top];
+	return elem[--top];	
+}
+
+template<typename T>
+inline T ArrayStack<T>::peek() const{
+	return elem[top - 1];
+}
+
+template<typename T>
+inline bool ArrayStack<T>::isEmpty() const{
+	return (top == 0);
 }
