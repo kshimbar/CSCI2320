146499b5e3196b46fe8e31aba6fca4ca58d822bb
kshimbar <kshimbar@trinity.edu>
2021-09-20 09:52:16 -0500

    Clean up

--- ArrayStack.h
+++ ArrayStack.h
@@ -5,34 +5,16 @@ class ArrayStack : public Stack<T> {
 public:
 	ArrayStack(): elem{new T[10]}, sz{10}, top{0} {};
 	ArrayStack(const ArrayStack &a);
-<<<<<<< HEAD
 	ArrayStack &operator=(const ArrayStack &a);
 	~ArrayStack() {delete[] elem;}
-=======
-    ArrayStack& operator=(const ArrayStack& a);
-	~ArrayStack() {}
-    ArrayStack& operator=(const ArrayStack& a){
-        T* p = new T[a.sz];
-        for(int i = 0; i != a.sz; ++i)
-                p[i] = a.elem[i];
-        delete[] elem;
-        elem = p;
-        sz = a.sz;
-        top = a.top;
-    }
-	~ArrayStack() {delete[] elem;
->>>>>>> 708d2940a8262b62f6224f9857c2fcccdb3bfcd0
 	void push(const T &t);
 	T pop();
 	T peek() const;
 	bool isEmpty() const;
-<<<<<<< HEAD
 private:
 	T *elem;
 	int sz;
 	int top;
-=======
->>>>>>> 708d2940a8262b62f6224f9857c2fcccdb3bfcd0
 };
 
 template<typename T>
@@ -53,7 +35,6 @@ ArrayStack<T> &ArrayStack<T>::operator=(const ArrayStack &a) {
 }
 
 template<typename T>
-<<<<<<< HEAD
 void ArrayStack<T>::push(const T &i) {
 	if (top == sz) {
 		T *tmp = new T[sz * 2];
@@ -80,51 +61,4 @@ inline T ArrayStack<T>::peek() const{
 template<typename T>
 inline bool ArrayStack<T>::isEmpty() const{
 	return (top == 0);
-=======
-ArrayStack<T> &ArrayStack<T>::operator=(const ArrayStack &a){
-    T* p = new T[a.sz];
-    for(int i = 0; i != a.sz; i++){
-        p[i] = a.elem[i];
-    }
-    delete[] elem;
-    elem = p;
-    sz = a.sz;
-    top = a.top;
-}
-
-template<typename T>
-void ArrayStack<T>::push(const T &t){
-    if(top == sz){
-       T* tmp = new T[sz*2];
-       for(int i = 0; i != sz; ++i){
-            tmp[i] = elem[i];
-        }
-        delete[] elem;
-        elem = tmp;
-        sz *= 2;
-    }
-    elem[top] = t;
-    top += 1;
-}
-
-<<<<<<< HEAD
-template<typename T>
-T ArrayStack<T>::pop() {
-	return elem[--top];
-}
-
-template<typename T>
-inline T ArrayStack<T>::peek() const {
-	return (elem[top - 1]);
-}
-
-template<typename T>
-bool ArrayStack<T>::isEmpty() const {
-    return (top == 0);
-=======
-template <typename T>
-T ArrayStack<T>::pop(){
-    return elem[--top];
->>>>>>> 52c9481b686df131d7f1f575b8a9e775bf610d00
->>>>>>> 708d2940a8262b62f6224f9857c2fcccdb3bfcd0
 }
